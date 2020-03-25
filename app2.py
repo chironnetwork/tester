5dc5c2b2b37aed8115e84be5f088a5f678c8ec2a
Anudit Nagar <nagaranudit@gmail.com>
2020-03-25 19:07:48 +0530

    Update tester

--- app2.py
+++ app2.py
@@ -5,7 +5,9 @@ import pickle
 from flask import Flask , render_template ,request, jsonify
 app = Flask(__name__)
 
-
+'''
+Train Model before starting Up the Server
+'''
 def data_split(data, ratio):
     np.random.seed(42)
     shuffled = np.random.permutation(len(data))
@@ -13,20 +15,6 @@ def data_split(data, ratio):
     test_indices = shuffled[:test_set_size]
     train_indices = shuffled[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
-
-@app.route('/', methods = ["GET","POST"])
-def check_result():
-
-    inputFeatures = [
-        int(request.args.get('fever')),
-        int(request.args.get('age')),
-        int(request.args.get('tiredness')),
-        int(request.args.get('cough')),
-        int(request.args.get('feverDays')),
-    ]
-    print(inputFeatures)
-    infProb = clf.predict_proba([inputFeatures])[0][1]
-    return jsonify({'result':infProb*100})
 
 df = pd.read_csv('data1.csv')
 train, test = data_split(df, 0.3)
@@ -39,4 +27,22 @@ Y_test = test[['infectionProb']].to_numpy().reshape(len(test),)
 clf = LogisticRegression()
 clf.fit(X_train, Y_train)
 
-app.run()
+@app.route('/', methods = ["GET","POST"])
+def check_result():
+
+    inputFeatures = [
+        int(request.args.get('fever')),
+        int(request.args.get('age')),
+        int(request.args.get('tiredness')),
+        int(request.args.get('cough')),
+        int(request.args.get('feverDays')),
+    ]
+    # print(inputFeatures)
+    infProb = clf.predict_proba([inputFeatures])[0][1]
+    return jsonify({'result':infProb*100})
+
+
+if __name__ == '__main__':
+
+    print("Server Up")
+    app.run()
